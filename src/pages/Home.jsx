187aa7efdcca3520b4c7d43de795147304abfc1f
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-10 19:56:56 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import StoryPage3 from "../sections/story/StoryPage3";
 import WordGameWithCategories from "../sections/alphabet/WordGameWithCategories";
+import SpellingGame from "../sections/alphabet/SpellingGame";
 
 export default function Home() {
   return (<>
   <div >
-    <StoryPage3 groupKey="alefba" wantedTitle="گُلدانِ نازنین" />
-    <WordGameWithCategories initialCategory="وسایل مدرسه" />
+    <h5 className="p-4 bg-warning m-4 rounded text-center">بچه ها از روی کلمات وسایل مدرسه با معنی انگلیسی آن یک بار بنویسندو همین طور فعالیت آن را انجام دهند. </h5>
+    <SpellingGame/>
+    <StoryPage3 groupKey="alefba" wantedTitle="رِسـتوران" />
+    {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
   </div>
 
   </>)
