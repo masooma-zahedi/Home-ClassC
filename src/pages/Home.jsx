b11b1935733bebd49fdddc7c12416f654fa1c727
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-18 14:20:48 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import StoryPage3 from "../sections/story/StoryPage3";
 import WordGameWithCategories from "../sections/alphabet/WordGameWithCategories";
 import SpellingGame from "../sections/alphabet/SpellingGame";
+import SentenceObject from "../sections/word/SentenceObject";
 
 export default function Home() {
   return (<>
   <div >
-    <h5 className="p-4 bg-warning m-4 rounded text-center">بچه ها از روی کلمات وسایل مدرسه با معنی انگلیسی آن یک بار بنویسندو همین طور فعالیت آن را انجام دهند. </h5>
-    <SpellingGame/>
-    <StoryPage3 groupKey="alefba" wantedTitle="رِسـتوران" />
+    <h5 className="p-4 bg-warning m-4 rounded text-center"> بچه ها از روی جملات بخوانند و از روی 5 جمله به دلخواه بنویسند</h5>
+    <SentenceObject/>
+    {/* <SpellingGame/> */}
+    {/* <StoryPage3 groupKey="alefba" wantedTitle="رِسـتوران" /> */}
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
   </div>
 
