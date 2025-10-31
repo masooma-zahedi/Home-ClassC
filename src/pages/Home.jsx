e68c981f85497e62abf2081883060e690d6b6122
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-30 19:28:50 -0500

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import StoryPage3 from "../sections/story/StoryPage3";
 import WordGameWithCategories from "../sections/alphabet/WordGameWithCategories";
 import SpellingGame from "../sections/alphabet/SpellingGame";
 import SentenceObject from "../sections/word/SentenceObject";
+import ObjectSentenceGame from "../sections/alphabet/ObjectSentenceGame";
 
 export default function Home() {
   return (<>
   <div >
-    <h5 className="p-4 bg-warning m-4 rounded text-center">بچه ها اسامی شغل ها را یاد بگیرند و از روی آن یک بار بنویسند. داستان را هم یکبار بخوانند. </h5>
+    <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها جملات زیر را تمرین کنند و از روی 7 جمله جواب درست آن بنویسند.  </div>
     {/* <SentenceObject/> */}
-    <SpellingGame/>
-    <StoryPage3 groupKey="alefba" wantedTitle="رِسـتوران" />
+    <ObjectSentenceGame/>
+    {/* <SpellingGame/> */}
+    {/* <StoryPage3 groupKey="alefba" wantedTitle="رِسـتوران" /> */}
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
   </div>
 
