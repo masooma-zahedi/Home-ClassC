775fe2751bf1abbc5838e67a4da7c4070d275553
masooma zahedi <masooma.zahedi1@gmail.com>
2025-11-07 19:13:14 -0600

    ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,21 @@ import WordGameWithCategories from "../sections/alphabet/WordGameWithCategories"
 import SpellingGame from "../sections/alphabet/SpellingGame";
 import SentenceObject from "../sections/word/SentenceObject";
 import ObjectSentenceGame from "../sections/alphabet/ObjectSentenceGame";
+import VideoCard from "../sections/alphabet/VideoCard";
 
 export default function Home() {
   return (<>
   <div >
     <div className="h5 m-5 border border-3 border-warning p-4 text-center bg-info rounded">بچه ها جملات زیر را تمرین کنند و از روی 7 جمله جواب درست آن بنویسند.  </div>
     {/* <SentenceObject/> */}
-    <ObjectSentenceGame/>
-    {/* <SpellingGame/> */}
+    {/* <ObjectSentenceGame/> */}
+    <VideoCard
+      title="قصه کوتاه امروز"
+      description="متن فارسی داستان زیر را بخوانید."
+      videoFileName={`${process.env.PUBLIC_URL}/video/chick.mp4`}  // فقط اسم فایل ویدیوی mp4
+    />
+
+    <SpellingGame/>
     {/* <StoryPage3 groupKey="alefba" wantedTitle="رِسـتوران" /> */}
     {/* <WordGameWithCategories initialCategory="وسایل مدرسه" /> */}
   </div>
